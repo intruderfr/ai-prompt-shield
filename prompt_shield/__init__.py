506b98219936c6b779f91ef48fd2c86dd3e17571
Aslam Ahamed <aslamahamed47@gmail.com>
2026-03-12 20:25:38 +0400

    feat: add package init

---
+++ prompt_shield/__init__.py
@@ -0,0 +1,4 @@
+from .shield import PromptShield, AnalysisResult, PromptInjectionError, INJECTION_PATTERNS
+
+__version__ = "0.1.0"
+__all__ = ["PromptShield", "AnalysisResult", "PromptInjectionError", "INJECTION_PATTERNS"]
